355c54973aded9a01af6ec5fff8d11d6da446355
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-06-22 11:39:45 +0530

    Add file upload

--- routes/hostels.js
+++ routes/hostels.js
@@ -4,17 +4,20 @@ const hostels = require('../controllers/hostels');
 const asyncError = require('../utils/asyncError');
 const { isLoggedIn, isAuthor, validateHostel } = require('../middleware');
 
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 const Hostel = require('../models/hostel');
 
 router.route('/')
     .get(asyncError(hostels.index))
-    .post(isLoggedIn, validateHostel, asyncError(hostels.createHostel))
-
+    .post(isLoggedIn, upload.array('image'), validateHostel, asyncError(hostels.createHostel))
+    
 router.get('/new', isLoggedIn, hostels.renderNewForm)
 
 router.route('/:id')
     .get(asyncError(hostels.showHostel))
-    .put(isLoggedIn, isAuthor, validateHostel, asyncError(hostels.updateHostel))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateHostel, asyncError(hostels.updateHostel))
     .delete(isLoggedIn, isAuthor, asyncError(hostels.deleteHostel));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, asyncError(hostels.renderEditForm))
