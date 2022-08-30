0fa65daf669190b90f0ee265a07813f496fce784
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2022-08-30 16:54:02 +0530

    Minor configuration

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-if(process.env.NODE_ENV !== "production"){
-    require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
 }
-
 
 const express = require('express');
 const path = require('path');
@@ -15,39 +14,63 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes = require('./routes/users');
 const hostelRoutes = require('./routes/hostels');
 const reviewRoutes = require('./routes/reviews');
 
-mongoose.connect('mongodb://localhost:27017/hostel-app');
+const MongoStore = require('connect-mongo');
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/hostel-app';
+
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
 });
 
 const app = express();
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+	mongoSanitize({
+		replaceWith: '_',
+	})
+);
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+	mongoUrl: dbUrl,
+	secret,
+	touchAfter: 24 * 60 * 60,
+});
+
+store.on('error', function (e) {
+	console.log('Session Store ERROR', e);
+});
 
 const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}
+	store,
+	secret,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {
+		httpOnly: true,
+		expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+		maxAge: 1000 * 60 * 60 * 24 * 7,
+	},
+};
 
 app.use(session(sessionConfig));
 app.use(flash());
@@ -60,11 +83,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    console.log(req.session);
-    res.locals.currentUser = req.user;
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
+	console.log(req.session);
+	res.locals.currentUser = req.user;
+	res.locals.success = req.flash('success');
+	res.locals.error = req.flash('error');
+	next();
 });
 
 app.use('/', userRoutes);
@@ -72,19 +95,19 @@ app.use('/hostels', hostelRoutes);
 app.use('/hostels/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
-    res.render('home')
+	res.render('home');
 });
 
 app.all('*', (req, res, next) => {
-    next(new ExpressError("Page not found!", 404))
-})
+	next(new ExpressError('Page not found!', 404));
+});
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if(!err.message) err.message = "Something went wrong!"
-    res.status(statusCode).render('error', { err })
-})
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Something went wrong!';
+	res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
-    console.log('serving on port: 3000')
+	console.log('serving on port: 3000');
 });
