ff590059898ddd375759130298ad19383f02cd36
Sandeep Sharma <sandeepsharma.4994@gmail.com>
2024-03-10 14:32:53 +0530

    Rename

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const reviewRoutes = require('./routes/reviews');
 const MongoStore = require('connect-mongo');
 
 const port = process.env.PORT || 3000;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/hostel-app';
+const dbUrl = process.env.DB_URL;
 
 mongoose.connect(dbUrl);
 
